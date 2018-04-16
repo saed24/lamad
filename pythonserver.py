8e47b00545199ba721ecca923570cbbe22d407b9
saeed24 <abu.saed.sayem@gmail.com>
2018-04-16 10:10:07 +0300

    Added handler.py

--- pythonserver.py
+++ pythonserver.py
@@ -2,6 +2,7 @@ import sys
 
 from flask import Flask, render_template, request, redirect, Response, jsonify, make_response
 import random, json
+import handler as handler
 
 app = Flask(__name__)
 
@@ -11,28 +12,12 @@ def output():
 	return render_template('index.html')
 
 @app.route('/get_route')
-def background_process():
+def returnRoute():
     try:
         lang = request.args.get('request_type', 0, type=str)
-        filepath = '1365683861651'
-        routes = []
-        with open(filepath) as fp:
-            line = fp.readline()
-            cnt = 1
-            while line:
-                points = {}
-                line = fp.readline()
-                temp = line.split(' ')
-                
-                if len(temp) == 4:
-                    points['lat'] = float(temp[0])
-                    points['lng'] = float(temp[1])
-                    points['time'] = temp[2]
-                    points['alt'] = temp[3]
-                    print(points);
-                    json_row = json.dumps(points)
-                    routes.append(points)
-               
+        
+        routes = handler.readRouteFromFile()
+        
         return jsonify(result=routes)	
     
     except Exception as e:
