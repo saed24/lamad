38019fcf3e57086e449c0a7891dac6c7b72ecaef
saeed24 <abu.saed.sayem@gmail.com>
2018-04-19 00:02:47 +0300

    Added functions to handler.py

--- handler.py
+++ handler.py
@@ -4,9 +4,10 @@ Created on Mon Apr 16 09:49:34 2018
 
 @author: Saeed
 """
-
+from math import sin, cos, sqrt, atan2, radians
 import os
 
+R = 6373.0 # approximate radius of earth in km
 
 def readRouteFromFile():
     filepath = '1216652978620'
@@ -51,3 +52,39 @@ def readRoutesFromFolder():
                     #print(points);
                     routes2.append(points)
     return routes2
+
+def Distance(lat1,lon1,lat2,lon2): # Calculates distance between two points
+    
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+    
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
+
+def PathDistance(routes): # Calculates Total distance of the path
+    total_distance=0
+
+    for i,obj in enumerate(routes[:-1]):
+        lat1=obj['lat']
+        lon1=obj['lng']
+        lat2=routes[i+1]['lat']
+        lon2=routes[i+1]['lng']
+        total_distance=total_distance+Distance(lat1,lon1,lat2,lon2)
+    return total_distance
+
+
+def DividingRoute(routes, A, B):
+    A=int (len(routes)*(A/100))
+    B=int (len(routes)*(B/100))
+    return routes[A:B]
+
+
