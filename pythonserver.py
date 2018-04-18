f6ddc4ec925d216ef06128491bd68be1c122b0de
Raheel Manoor <m.raheelmansoor@gmail.com>
2018-04-18 10:49:44 +0300

    Some error with git

--- pythonserver.py
+++ pythonserver.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import sys
+
+from flask import Flask, render_template, request, redirect, Response, jsonify, make_response
+import random, json
+import handler as handler
+
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "Hello World!"
- 
+
+@app.route('/')
+def output():
+	# serve index template
+	return render_template('index.html')
+
+@app.route('/get_route')
+def returnRoute():
+    try:
+        lang = request.args.get('request_type', 0, type=str)
+        
+        routes = handler.readRouteFromFile()
+        
+        return jsonify(result=routes)	
+    
+    except Exception as e:
+		   return str(e)
+
 if __name__ == "__main__":
     app.run()
