d9ef88b215f7443d1d136dd60b2786ff76981cca
Raheel Manoor <m.raheelmansoor@gmail.com>
2018-04-18 19:44:10 +0300

    Added the DistancePath() method to Distance.py and created DividingRoute.py

--- Distance.py
+++ Distance.py
@@ -9,7 +9,7 @@ from math import sin, cos, sqrt, atan2, radians
 R = 6373.0 # approximate radius of earth in km
 
 
-def Distance(lat1,lon1,lat2,lon2):
+def Distance(lat1,lon1,lat2,lon2): # Calculates distance between two points
     
     lat1 = radians(lat1)
     lon1 = radians(lon1)
@@ -24,3 +24,14 @@ def Distance(lat1,lon1,lat2,lon2):
 
     distance = R * c
     return distance
+
+def PathDistance(routes): # Calculates Total distance of the path
+    total_distance=0
+
+    for i,obj in enumerate(routes[:-1]):
+        lat1=obj['lat']
+        lon1=obj['lng']
+        lat2=routes[i+1]['lat']
+        lon2=routes[i+1]['lng']
+        total_distance=total_distance+Distance(lat1,lon1,lat2,lon2)
+    return total_distance
--- testing distance.py
+++ testing distance.py
@@ -9,17 +9,11 @@ import handler as handler
 import Distance as Distance
 
 routes = handler.readRouteFromFile()
-initial_long=routes[1]['lng']
-initial_lat=routes[1]['lat']
-total_distance=0
-
-for i,obj in enumerate(routes[:-1]):
-    #print(i['lat'],i['lng'])
-   # j=i+1
-    lat1=obj['lat']
-    lon1=obj['lng']
-    lat2=routes[i+1]['lat']
-    lon2=routes[i+1]['lng']
-    total_distance=total_distance+Distance.Distance(lat1,lon1,lat2,lon2)
-
+total_distance=Distance.PathDistance(routes)
 print(total_distance)
+
+
+print(len(routes))
+
+print(routes[:5])
+print list1[-5:]
